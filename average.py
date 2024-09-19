241666fd5b5782b219f5fc9e875d7ebc7579be81
ragul_gopinath <150418890+Ragul-05@users.noreply.github.com>
2024-09-19 23:20:58 +0530

    Average problem

---
+++ average.py
@@ -0,0 +1,10 @@
+def call(a,b):
+    print("the value of a and b :",a,b)
+    avg=a+b/2
+    print("the average value is",avg)
+    return avg
+a=int(input("Enter the value of a:"))
+b=int(input("Enter the value of b:"))
+call(a,b)
+
+    
